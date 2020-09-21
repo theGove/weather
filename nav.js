9bbfa24552dfdf032ee4c8a4f0b160e66a65d072
thegove <gove.allen@gmail.com>
2020-09-21 15:09:50 -0600

    read date from ULR

--- nav.js
+++ nav.js
@@ -2,9 +2,13 @@
 function start_me_up(){
     const current_site = get_site()
     console.log("sites",sites[current_site][3])
+    console.log("date",get_date())
+    
     var di = document.getElementById('date');
     di.setAttribute("min", sites[current_site][2])
     di.setAttribute("max", sites[current_site][3])
+    //di.setAttribute("value", "2018-05-02")
+    di.value=get_date()
     console.log("di", di)
 
     const nav=document.getElementById("nav-div")
@@ -54,5 +58,22 @@ function get_site(){
         }
     }
     if(n>-1){return a[n-1]}
+
+}
+
+function get_date(){
+    const a = window.location.href.split('/')
+    //console.log(a)
+    let n=-1
+    for(let x=0;x<a.length;x++){
+        if(a[x].length===4){
+            if(!isNaN(a[x])){
+               // we have found a four-digit number
+                n=x
+                break
+            } 
+        }
+    }
+    if(n>-1){return a[n] + "-" + ("0" + a[n+1]).slice(-2) + "-" +("0" + a[n+2]).slice(-2)  }
 
 }
